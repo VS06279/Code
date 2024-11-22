d04ec4a0db95251c3d859e953ec979ae8e4bfba3
VS06279 <vikrant.suryadevara@gmail.com>
2024-11-22 15:38:06 -0500

    Add files via upload

---
+++ Lab.java
@@ -0,0 +1,22 @@
+package lab;
+
+public class Lab {
+
+	public static void main(String[] args) {
+		// Vikrant Suryadevara
+		System.out.println("Drawing the initial shape:");
+		System.out.println(" _     _");
+		System.out.println("( `^  `)) ");
+		System.out.println("|      ||");
+		System.out.println("|      ||");
+		System.out.println("' ---- '` ");
+		
+		System.out.println("Displaying 2 copies of the shape that are horizontally adjacent:");
+		System.out.println(" _     _      _     _");
+		System.out.println("( `^  `))    ( `^  `)) ");
+		System.out.println("|      ||    |      ||");
+		System.out.println("|      ||    |      ||");
+		System.out.println("' ---- '`    ' ---- '`");
+	}
+
+}
